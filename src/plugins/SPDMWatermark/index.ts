164f6818197cb1032dd25220b81c675233351948
lockieluke <sherlockluk@outlook.com>
2021-06-11 16:55:00 +0800

    Fixed the issue that clicking watermark disables reload

--- src/plugins/SPDMWatermark/index.ts
+++ src/plugins/SPDMWatermark/index.ts
@@ -13,8 +13,7 @@ export default class SPDMWatermark implements SpotifyPlugin {
     public load() {
         SpotifyEventListener.once('playlist-folders-added', () => {
             const message: string = "Powered By SpotifyDesktopMod";
-            const href: string = "#";
-            const basedom: string = `<li class="GlueDropTarget GlueDropTarget--playlists GlueDropTarget--folders"><div class="c9c91e3ac3f8007893010e4484c2ef7d-scss" draggable="true" data-testid="rootlist-item" style="--indentation:0;"><a class="standalone-ellipsis-one-line _9a78420cc4a863b2f413ce55e759a321-scss" draggable="false" tabindex="-1" href="${href}"><span class="bd0f04911fe4adb022e666269a90a739-scss _2002bac6ea2fc1db22e491bd0d24f1ee-scss" as="span" dir="auto">${message}</span></a><div class="ea5753b342cd7d8290d6c85d3978456b-scss"></div></div></li>`;
+            const basedom: string = `<li class="GlueDropTarget GlueDropTarget--playlists GlueDropTarget--folders"><div class="c9c91e3ac3f8007893010e4484c2ef7d-scss" draggable="true" data-testid="rootlist-item" style="--indentation:0;"><a class="standalone-ellipsis-one-line _9a78420cc4a863b2f413ce55e759a321-scss" draggable="false" tabindex="-1"><span class="bd0f04911fe4adb022e666269a90a739-scss _2002bac6ea2fc1db22e491bd0d24f1ee-scss" as="span" dir="auto">${message}</span></a><div class="ea5753b342cd7d8290d6c85d3978456b-scss"></div></div></li>`;
 
             $(basedom).attr('title', "This Spotify Desktop instance is modded with SpotifyDesktopMod which enables plugins and themes to the client").css({
                 'overflow-wrap': 'break-word',
