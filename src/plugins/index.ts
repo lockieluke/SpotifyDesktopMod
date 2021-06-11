26edda22ff76eee21dc8f5075cfecf13063e420a
lockieluke <sherlockluk@outlook.com>
2021-06-11 13:36:15 +0800

    Enabled plugin

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -18,7 +18,7 @@ export default class SpotifyPluginLoader {
     private static plugins: SpotifyPlugin[] = [];
 
     public static loadPlugins() {
-        this.plugins.push(new HideUpgradeButton());
+        // this.plugins.push(new HideUpgradeButton());
         // this.plugins.push(new HideFriendPane());
         this.plugins.push(new SPDMWatermark());
         this.plugins.push(new RemoveAdiFrames());
