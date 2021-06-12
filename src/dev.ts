fb6f0915c08fcb8cbbd1c2dac55905a33b20babb
lockieluke <sherlockluk@outlook.com>
2021-06-12 18:40:21 +0800

    Removed original reload function in favour of Spotify DevMode

--- src/dev.ts
+++ src/dev.ts
@@ -1,6 +1,5 @@
 import { getSpotifyDOM, SpotifyDOM } from "./dom/spDomLib";
 import $ = require('jquery');
-import platform = require('platform');
 
 export default class Development {
 
@@ -10,15 +9,6 @@ export default class Development {
 
     public static initDevelopment() {
         if (!this.ifDev()) return;
-
-        // Reload script with dev-server
-        $(document.body).on('keydown', event => {
-            const isMac: boolean = platform.os.family === 'OS X';
-            if (event.key === 'g' && (isMac ? event.metaKey : event.ctrlKey)) {
-                event.preventDefault();
-                window.location.reload();
-            }
-        })
     }
 
 }
